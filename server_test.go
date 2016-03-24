fdc825a4137ece60c27eff88ab271998951ced78
Josh Baker <joshbaker77@gmail.com>
2016-03-24 06:10:19 -0700

    conncurrent client test

--- server_test.go
+++ server_test.go
@@ -1,8 +1,10 @@
 package resp
 
 import (
+	"fmt"
 	"net"
 	"os"
+	"sync"
 	"testing"
 	"time"
 )
@@ -16,59 +18,75 @@ func TestServer(t *testing.T) {
 		select {}
 	}
 	time.Sleep(time.Millisecond * 50)
-	nconn, err := net.Dial("tcp", ":6380")
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer nconn.Close()
-	conn := NewConn(nconn)
 
-	// PING
-	if err := conn.WriteMultiBulk("PING"); err != nil {
-		t.Fatal(err)
-	}
-	val, err := conn.ReadValue()
-	if err != nil {
-		t.Fatal(err)
-	}
-	if val.String() != "PONG" {
-		t.Fatalf("expecting 'PONG', got '%s'", val)
-	}
+	n := 75
 
-	// SET
-	if err := conn.WriteMultiBulk("SET", "test", 123.4); err != nil {
-		t.Fatal(err)
-	}
-	val, err = conn.ReadValue()
-	if err != nil {
-		t.Fatal(err)
-	}
-	if val.String() != "OK" {
-		t.Fatalf("expecting 'OK', got '%s'", val)
-	}
+	// Open N connections and do a bunch of stuff.
+	var wg sync.WaitGroup
+	wg.Add(n)
+	for i := 0; i < n; i++ {
+		go func(i int) {
+			defer func() {
+				wg.Done()
+			}()
+			nconn, err := net.Dial("tcp", ":6380")
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer nconn.Close()
+			conn := NewConn(nconn)
 
-	// GET
-	if err := conn.WriteMultiBulk("GET", "test"); err != nil {
-		t.Fatal(err)
-	}
-	val, err = conn.ReadValue()
-	if err != nil {
-		t.Fatal(err)
-	}
-	if val.Float() != 123.4 {
-		t.Fatalf("expecting '123.4', got '%s'", val)
-	}
+			// PING
+			if err := conn.WriteMultiBulk("PING"); err != nil {
+				t.Fatal(err)
+			}
+			val, err := conn.ReadValue()
+			if err != nil {
+				t.Fatal(err)
+			}
+			if val.String() != "PONG" {
+				t.Fatalf("expecting 'PONG', got '%s'", val)
+			}
 
-	// QUIT
-	if err := conn.WriteMultiBulk("QUIT"); err != nil {
-		t.Fatal(err)
-	}
-	val, err = conn.ReadValue()
-	if err != nil {
-		t.Fatal(err)
-	}
-	if val.String() != "OK" {
-		t.Fatalf("expecting 'OK', got '%s'", val)
-	}
+			key := fmt.Sprintf("key:%d", i)
 
+			// SET
+			if err := conn.WriteMultiBulk("SET", key, 123.4); err != nil {
+				t.Fatal(err)
+			}
+			val, err = conn.ReadValue()
+			if err != nil {
+				t.Fatal(err)
+			}
+			if val.String() != "OK" {
+				t.Fatalf("expecting 'OK', got '%s'", val)
+			}
+
+			// GET
+			if err := conn.WriteMultiBulk("GET", key); err != nil {
+				t.Fatal(err)
+			}
+			val, err = conn.ReadValue()
+			if err != nil {
+				t.Fatal(err)
+			}
+			if val.Float() != 123.4 {
+				t.Fatalf("expecting '123.4', got '%s'", val)
+			}
+
+			// QUIT
+			if err := conn.WriteMultiBulk("QUIT"); err != nil {
+				t.Fatal(err)
+			}
+			val, err = conn.ReadValue()
+			if err != nil {
+				t.Fatal(err)
+			}
+			if val.String() != "OK" {
+				t.Fatalf("expecting 'OK', got '%s'", val)
+			}
+
+		}(i)
+	}
+	wg.Wait()
 }
